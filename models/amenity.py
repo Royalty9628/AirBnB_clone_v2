6cab7d9888238738290d2aa738bdf4ce9413acdf
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2023-01-05 08:59:13 -0800

    DB Storage File

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    '''This is amenity class'''
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'amenities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary=place_amenity)
--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,107 +1,60 @@
 #!/usr/bin/python3
-"""test for databasse storage"""
+
 import unittest
-from os import getenv
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
 from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
-from models.engine.db_storage import DBStorage
-from models import storage
-import MySQLdb
-import pep8
+from models.amenity import Amenity
+from models.state import State
+from models.place import Place
+from models.city import City
+import os
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-class TestDBStorage(unittest.TestCase):
-    '''this will test the DBStorage'''
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                 "only testing db")
+class test_DBStorage(unittest.TestCase):
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def setUp(self):
-        """set up for test"""
-        if getenv("HBNB_TYPE_STORAGE") == "db":
-            self.db = MySQLdb.connect(getenv("HBNB_MYSQL_HOST"),
-                                      getenv("HBNB_MYSQL_USER"),
-                                      getenv("HBNB_MYSQL_PWD"),
-                                      getenv("HBNB_MYSQL_DB"))
-            self.cursor = self.db.cursor()
+    def test_State(self):
+        """ Check if loads name"""
+        state = State(name="Carl")
+        if state.id in models.storage.all():
+            self.assertTrue(state.name, "Carl")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def tearDown(self):
-        """at the end of the test this will tear it down"""
-        if getenv("HBNB_TYPE_STORAGE") == "db":
-            self.db.close()
+    def test_City(self):
+        """ Check if loads name """
+        city = City(name="Bogota")
+        if city.id in models.storage.all():
+            self.assertTrue(city.name, "Bogota")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_pep8_DBStorage(self):
-        """Testing for pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_Place(self):
+        """ Checks if leads name and numer """
+        place = Place(name="Hotel", number_rooms=4)
+        if place.id in models.storage.all():
+            self.assertTrue(place.number_rooms, 4)
+            self.assertTrue(place.name, "Hotel")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_attributes_DBStorage(self):
-        """Tests for class attributes"""
-        self.assertTrue(hasattr(DBStorage, '_DBStorage__engine'))
-        self.assertTrue(hasattr(DBStorage, '_DBStorage__session'))
-        self.assertTrue(hasattr(DBStorage, 'all'))
-        self.assertTrue(hasattr(DBStorage, 'new'))
-        self.assertTrue(hasattr(DBStorage, 'save'))
-        self.assertTrue(hasattr(DBStorage, 'delete'))
-        self.assertTrue(hasattr(DBStorage, 'reload'))
+    def test_User(self):
+        """ Check if loads name """
+        user = User(name="926")
+        if user.id in models.storage.all():
+            self.assertTrue(user.name, "926")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_all_DBStorage(self):
-        """Tests for all() method"""
-        s = State(name="California")
-        storage.new(s)
-        storage.save()
-        k = '{}.{}'.format(type(s).__name__, s.id)
-        dic = storage.all(State)
-        self.assertTrue(k in dic.keys())
-        s1 = State(name="Arizona")
-        storage.new(s1)
-        storage.save()
-        k1 = '{}.{}'.format(type(s1).__name__, s1.id)
-        dic1 = storage.all()
-        self.assertTrue(k in dic1.keys())
-        self.assertTrue(k1 in dic1.keys())
-        u = User(email="derps@herps.com", password="hurrdurr")
-        storage.new(u)
-        storage.save()
-        k2 = '{}.{}'.format(type(u).__name__, u.id)
-        dic2 = storage.all(User)
-        self.assertTrue(k2 in dic2.keys())
-        self.assertFalse(k1 in dic2.keys())
-        self.assertFalse(k in dic2.keys())
-        self.assertFalse(k2 in dic.keys())
+    def test_Amenity(self):
+        """ Check if loads name """
+        amenity = Amenity(name="Wifi")
+        if amenity.id in models.storage.all():
+            self.assertTrue(amenity.name, "Wifi")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_new_DBStorage(self):
-        """Tests for new() method"""
-        nb = self.cursor.execute("SELECT COUNT(*) FROM states")
-        s = State(name="Oregon")
-        s.save()
-        nb1 = self.cursor.execute("SELECT COUNT(*) FROM states")
-        self.assertEqual(nb1 - nb, 0)
+    def test_Review(self):
+        """ Tests if leads text """
+        review = Review(text="Very_good_place")
+        if review.id in models.storage.all():
+            self.assertTrue(review.text, "Very_good_place")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_reload(self):
-        """Test for reload()"""
-        obj = DBStorage()
-        self.assertTrue(obj._DBStorage__engine is not None)
-        self.assertTrue(obj._DBStorage__session is None)
-        obj.reload()
-        self.assertTrue(obj._DBStorage__session is not None)
-
-if __name__ == "__main__":
-    unittest.main()
+    def teardown(self):
+        self.session.close()
+        self.session.rollback()
