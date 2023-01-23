b5b015ff9769770bb0e7722940c2049798e3a703
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2023-01-23 07:12:51 -0800

    Deploy archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,15 +7,11 @@ from datetime import date
 
 
 def do_pack():
-    """Fabric Script Function to compress files"""
-
-     filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
+     """ A script that generates archive the contents of web_static folder"""
+     local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
         return None
+    return result
